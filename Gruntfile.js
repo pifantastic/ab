47a1f966b31dd5e5135f1d5fc92945c9d603f63b
Aaron Forsander <aforsander@rmn.com>
2014-02-28 14:20:23 -0600

    sauce/travis.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,15 @@ module.exports = function (grunt) {
 
     pkg: grunt.file.readJSON('package.json'),
 
+    connect: {
+      test: {
+        options: {
+          base: '',
+          port: 9999
+        }
+      }
+    },
+
     mocha: {
       options: {
         log: true,
@@ -19,11 +28,30 @@ module.exports = function (grunt) {
       }
     },
 
+    'saucelabs-mocha': {
+      test: {
+        options: {
+          urls: ['http://127.0.0.1:9999/tests/index.html'],
+          tunnelTimeout: 5,
+          build: process.env.TRAVIS_JOB_ID,
+          concurrency: 3,
+          browsers: [{
+            browserName: 'chrome',
+            platform: 'linux'
+          }],
+          testname: 'mocha tests',
+          tags: ['master']
+        }
+      }
+    }
+
   });
 
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-saucelabs');
   grunt.loadNpmTasks('grunt-mocha');
 
   grunt.registerTask('default', ['test']);
-  grunt.registerTask('test', ['mocha']);
+  grunt.registerTask('test', ['connect', 'saucelabs-mocha']);
 
 };
